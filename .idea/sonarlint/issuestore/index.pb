230b395ee37ead9ff5e924498ecf357a8b6a8cd1
Саша <galdovich95@gmail.com>
2020-06-20 13:24:46 +0300

    Final

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+a
+1src/com/galdovich/day2/service/BasketService.java,f\5\f564b144d659d669f061002a5b12a90d36d7e76e
+i
+9test/test/com/galdovich/day2/creator/BallCreatorTest.java,4\f\4fdb505bb54b1d6a7a058518a74adcf68112692e
---
+++ src/com/galdovich/day2/exception/CustomException.java
@@ -0,0 +1,19 @@
+package com.galdovich.day2.exception;
+
+public class CustomException extends Exception {
+
+    public CustomException() {
+    }
+
+    public CustomException(String message) {
+        super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomException(Throwable cause) {
+        super(cause);
+    }
+}
